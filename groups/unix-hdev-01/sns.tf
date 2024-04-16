2eb3285919e96a7c833bd7afa294a60d3cc1cb92
Chris Harris <charris1@companieshouse.gov.uk>
2024-04-16 09:22:29 +0100

    adding missing dash

--- groups/unix-hdev-01/sns.tf
+++ groups/unix-hdev-01/sns.tf
@@ -20,8 +20,8 @@ module "cloudwatch_sns_notifications_ooh" {
   source  = "terraform-aws-modules/sns/aws"
   version = "3.3.0"
 
-  name              = "unix-hdev-01cloudwatch-ooh"
-  display_name      = "unix-hdev-01cloudwatch-alarms-ooh-only"
+  name              = "unix-hdev-01-cloudwatch-ooh"
+  display_name      = "unix-hdev-01-cloudwatch-alarms-ooh-only"
   kms_master_key_id = local.sns_kms_key_id
 
   tags = merge(
