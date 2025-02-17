1fdc0cad2763fc88c16d06d80112da5bee606838
charris-CH <charris1@companieshouse.gov.uk>
2025-02-17 11:26:24 +0000

    updated dns record name

--- groups/linux-dev-03/dns.tf
+++ groups/linux-dev-03/dns.tf
@@ -1,7 +1,7 @@
-resource "aws_route53_record" "linux_dev_02" {
+resource "aws_route53_record" "linux_dev_03" {
   count = var.instance_count
 
-  zone_id = data.aws_route53_zone.linux_dev_02.zone_id
+  zone_id = data.aws_route53_zone.linux_dev_03.zone_id
   name    = "${var.service_subtype}"
   type    = "A"
   ttl     = 300
