929463f2ce93a1eb6725900da017c90974eea37e
charris-CH <charris1@companieshouse.gov.uk>
2025-03-05 08:52:51 +0000

    removed obselete tag

--- groups/linux-dev-02/instance.tf
+++ groups/linux-dev-02/instance.tf
@@ -16,7 +16,6 @@ resource "aws_instance" "linux_dev_02" {
     Backup         = true
     Domain         = "${var.environment}.${var.dns_zone_suffix}"
     Hostname       = "${var.service_subtype}"
-    OOH            = false
   }
 
   root_block_device {
